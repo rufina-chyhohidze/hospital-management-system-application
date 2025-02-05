206bcfeca0e47cf2f372747a88b7321fdbd22b96
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2025-02-05 13:06:33 +0100

    Initial commit

---
+++ src/main/java/org/example/programming5project/Programming5ProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.programming5project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programming5ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Programming5ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/programming5project/Programming5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.programming5project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programming5ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
